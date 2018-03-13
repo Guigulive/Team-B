7510a4a821db1ecb8711d584083234a994a85f64
JerichoX <jrcho17@163.com>
2018-03-13 22:18:06 +0800

    14_张继超_第1课作业
    添加的两个函数是updateEmployee()和updateSalary()，分别实现了修改地址和修改薪资的功能。

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,46 @@
 /*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint salaryUnit = 1 ether;
+    uint salary = 1;
+    address employee;
+    uint constant payDuration = 10 seconds;
+    uint lastPayday = now;
+    
+    //Update address
+    function updateEmployee(address e) {
+        employee = e;
+    }
+    
+    //Update salary
+    function updateSalary(uint s){
+        salary = s * salaryUnit;
+    }
+    
+    function addFund() payable returns (uint){
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway()>0;
+    }
+    
+    function getPaid() {
+        if(msg.sender != employee){
+            revert();
+        }
+        
+        uint nextPayDay = lastPayday + payDuration;
+        if(nextPayDay > now){
+            revert();
+        }
+        
+        lastPayday = lastPayday + payDuration;
+        employee.transfer(salary);
+    }
+}
