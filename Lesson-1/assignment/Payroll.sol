8cf8f147c8596f81c568f3d2137a23a8393506a9
gang <ligang@me.com>
2018-03-19 22:02:01 +0800

    revert master merge

--- Lesson-1/assignment/Payroll.sol
+++ Lesson-1/assignment/Payroll.sol
@@ -1,39 +1,56 @@
-pragma solidity ^0.4.14;
-
-contract Payroll{
-    uint salary;//薪水
-    address staff;//员工钱包地址
-    uint constant payDuration = 10 seconds;
-    uint lastPayday = now;
-    
-    function setStaff(address staffAddress){//设置员工账户地址
-        staff = staffAddress;
-    }
-    
-    function setSalary(uint staffSalary){//设置薪酬
-        salary = staffSalary;
-    }
-    
-    function addFund() payable returns (uint){//账户存钱
-        return this.balance;
-    }
-    
-    function calculateRunway() returns (uint){//账户中还能支付多少次薪酬
-        return this.balance/salary;
-    }
-    
-    function hasEnoughFund() returns (bool){//账户中是否还能支付一次薪酬
-        return calculateRunway() >0;
-    }
-    
-    function getPaid(){//给员工发工资
-        uint nextPayday = lastPayday+payDuration;
-        if(nextPayday>now || msg.sender != staff){//如果没到发工资或者拿钱的不是员工本人，抛出异常
-            revert();
-        }
-            lastPayday = nextPayday;//更新上次发薪时间
-            staff.transfer(salary);//给员工支付薪酬
+pragma solidity ^0.4.14; 
+ 
+contract Payroll { 
+    uint constant payDuration = 10 seconds; 
+ 
+    address owner; 
+    uint salary; 
+    address employee; 
+    uint lastPayday; 
+ 
+    function Payroll() { 
+        owner = msg.sender; 
+    } 
      
-    }
-    
-}
+    function updateEmployee(address e, uint s) returns (uint) { 
+        require(msg.sender == owner); 
+         
+        if (employee != 0x0) { 
+            uint payment = salary * (now - lastPayday) / payDuration; 
+            employee.transfer(payment); 
+        } 
+         
+        employee = e; 
+        salary = s * 1 ether; 
+        lastPayday = now; 
+        return salary; 
+    } 
+     
+    function updateSalary(uint s) returns (uint) { 
+        salary = s; 
+        return salary; 
+    } 
+    function addFund() payable returns (uint) { 
+        return this.balance; 
+    } 
+ 
+    function calculateRunway() returns (uint) { 
+        // return if salary not initialized 
+        if(salary != 0) 
+            return this.balance / salary; 
+    } 
+     
+    function hasEnoughFund() returns (bool) { 
+        return calculateRunway() > 0; 
+    } 
+     
+    function getPaid() { 
+        require(msg.sender == employee); 
+         
+        uint nextPayday = lastPayday + payDuration; 
+        assert(nextPayday < now); 
+ 
+        lastPayday = nextPayday; 
+        employee.transfer(salary); 
+    } 
+}         
--- Lesson-2/assignment/Payroll.sol
+++ Lesson-2/assignment/Payroll.sol
@@ -1,123 +1,70 @@
 pragma solidity ^0.4.14;
-
-/*
-*雇主:0xca35b7d915458ef540ade6068dfe2f44e8fa733c
-*
-*员工1:0x14723a09acff6d2a60dcdf7aa4aff308fddc160c    
-*transaction cost 22966    execution cost 1694
-*员工2:0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db
-*transaction cost 23747    execution cost 2475
-*员工3:0x583031d1113ad414f02576bd6afabfb302140225
-*transaction cost 24528    execution cost 3256
-*员工4:0xdd870fa1b7c4700f2bd7f44238821c26f7392148
-*transaction cost 25309    execution cost 4037
-*员工5:0xe4829bcf2f0979522ba7afe74f9fb7dc41322a6d
-*transaction cost 26090    execution cost 4818
-*员工6:0x358a7ce1782fc82900ff9e351f7e78c90e5adb4a
-*transaction cost 26871    execution cost 5599
-*员工7:0xf7c3c48d00983d179f876098781cebfc0eb6a130
-*transaction cost 27652    execution cost 6380
-*员工8:0x0a451ba2621db2ffe257cb53580e481ef02e3fe0
-*transaction cost 28433    execution cost 7161
-*员工9:0xfb364d47f57d8cf6e569769cca06f3771759fe75
-*transaction cost 29214    execution cost 7942
-*员工10:0x113c2c0d6ce45d10ca06ff600fa06bda07d103a2
-*transaction cost 29995    execution cost 8723
-*
-*观察发现每加入一个员工，计算剩余支付薪酬消耗的gas就越多，因为雇员的数量越多遍历计算的就越多
-*优化calculateRunway函数，新定义状态变量totalSalary，在新增、删除、更新雇员过程中动态更新totalSalary，最后执行calculateRunway函数时就避免计算，直接算剩余支付薪酬
-*/
-
-
 contract Payroll {
-    struct Employee{
-        address id;//合约雇员的id
-        uint salary;//合约薪酬
-        uint lastPayday;//上次付款薪酬
-        
+    struct Employee {
+        address id;
+        uint salary;
+        uint lastPayday;
     }
-
-    uint constant payDuration = 10 seconds;// 发薪周期
-
-    address owner;//合约拥有者
-    Employee[] employees;
-    
-    uint totalSalary=0;//初始化总薪酬等于0
-
-    function Payroll() {
-        owner = msg.sender;//
+    uint totalSalary;
+    uint constant payDuration = 10 seconds;
+ 
+    address  owner;
+    Employee[] public employees;
+ 
+    function Payroll() payable {
+        owner = msg.sender;
     }
     
     function _partialPaid(Employee employee) private {
-        uint payment = employee.salary*(now-employee.lastPayday)/payDuration;
-        employee.id.transfer(payment);
+        require(employee.id != 0);
+        employee.id.transfer(employee.salary * (now - employee.lastPayday) / payDuration);
     }
     
-    function _findEmployee(address employeeId) private returns (Employee,uint){
-        for(uint i = 0;i < employees.length;i++){
-            if(employees[i].id == employeeId){
-                return (employees[i],i);
+    function _findEmployee(address employeeId) private returns (Employee, uint) {
+        for(uint i = 0; i < employees.length; i++) {
+            Employee e = employees[i];
+            if(e.id == employeeId) {
+                return (e, i);
             }
         }
     }
-    
-    function addEmployee(address employeeId,uint salary){
-        require(msg.sender==owner);
-        
-        var(employee,index) = _findEmployee(employeeId);//有没有办法像这样共通的employee不能为空的判断，在遍历employees的时候就判断，并对外抛出异常，使外层调用的函数也能终止，避免重复判断规则。
-        assert(employee.id == 0x0);
-        
-        employees.push(Employee(employeeId,salary * 1 ether,now));
-        
-        totalSalary += salary * 1 ether; 
+    function addEmployee(address employeeId, uint salary) {
+        require(msg.sender == owner);
+        var (e, index)= _findEmployee(employeeId);
+        assert(e.id == 0);
+        employees.push(Employee(employeeId, salary * 1 ether, now));
+        totalSalary += salary * 1 ether;
     }
     
-    function removeEmployee(address employeeId){
-        require(msg.sender==owner);
-        
-        var(employee,index) = _findEmployee(employeeId);
-        assert(employee.id != 0x0);
-        
-         _partialPaid(employee);
-         
-         totalSalary -= employee.salary; 
-         
-         delete employees[index];
-        employees[index]=employees[employees.length-1];
-        employees.length-=1;
-        
-        
+    function removeEmployee(address employeeId) {
+        require(msg.sender == owner);
+        var (e, i) = _findEmployee(employeeId);
+        assert(e.id != 0);
+        _partialPaid(e);
+        delete employees[i];
+        employees[i] = employees[employees.length - 1];
+        employees.length -= 1;
+        totalSalary -= e.salary;
     }
     
     function updateEmployee(address employeeId, uint salary) {
-        require(msg.sender == owner);//更新员工和薪资
-        
-        var(employee,index) = _findEmployee(employeeId);
-        assert(employee.id != 0x0);
-        
-        _partialPaid(employee);
-        
-        totalSalary -= employee.salary; 
-        totalSalary += salary * 1 ether; 
-        
+        require(msg.sender == owner);
+        var (e, index) = _findEmployee(employeeId);
+        assert(e.id != 0);
+        _partialPaid(e);
+        totalSalary -= e.salary;
         employees[index].salary = salary * 1 ether;
-        employees[index].lastPayday = now;
-        
-
-
+        totalSalary += employees[index].salary;
     }
     
     function addFund() payable returns (uint) {
+        require(msg.sender == owner);
         return this.balance;
     }
     
-    function calculateRunway() returns (uint) {//这里是固定薪酬的总和，当有人已支付过不是一个完整周期的薪酬，这里计算的方式就有问题。
-        //uint totalSalary = 0;
-        //for(uint i = 0;i < employees.length;i++){
-            //totalSalary += employees[i].salary; 
-        //}
-        
-        return this.balance / totalSalary;
+    function calculateRunway() returns (uint) {
+        if(totalSalary > 0)    
+            return this.balance / totalSalary;
     }
     
     function hasEnoughFund() returns (bool) {
@@ -125,14 +72,17 @@ contract Payroll {
     }
     
     function getPaid() {
-         var(employee,index) = _findEmployee(msg.sender);
-        assert(employee.id != 0x0);
-        
-        uint nextPayday = employee.lastPayday + payDuration;
-        assert(nextPayday < now);
-        
-        employees[index].lastPayday = nextPayday;
-        employee.id.transfer(employee.salary);
-
+        var (employee, index) = _findEmployee(msg.sender);
+        assert(employee.id != 0);
+        _partialPaid(employee);
+    }
+    function check() returns (uint) {
+        return employees.length;
+    }
+    function checkSalary() returns(uint) {
+        return employees[0].salary;
+    }
+    function getOwner() returns (address){
+        return owner;
     }
 }
