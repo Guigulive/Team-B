e3d3193a41b1a7a42bf5d3d75393461eccc2de65
gang <ligang@me.com>
2018-04-04 00:41:30 +0800

    save temp

--- Lesson-7/assignment/migrations/2_deploy_contracts.js
+++ Lesson-7/assignment/migrations/2_deploy_contracts.js
@@ -3,7 +3,9 @@ var SafeMath = artifacts.require("./SafeMath.sol");
 var Payroll = artifacts.require("./Payroll.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Ownable);
+  console.log('delply 1');
+  deployer.deploy(Ownable, {gas: 6721975});
+  console.log('delply 2');  
   deployer.deploy(SafeMath);
 
   deployer.link(Ownable, Payroll);
--- Lesson-7/assignment/truffle.js
+++ Lesson-7/assignment/truffle.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = 'omit wealth film sunny faculty priority endless panel energy verb tree deer';
+
 module.exports = {
   migrations_directory: "./migrations",
   networks: {
@@ -5,6 +8,13 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/8Crf9sXudSYJFZjsJz97"),
+      gas: 30000000,
+      from: "0x2DA1BC462B4D6eFdfa03EaF8266Aee03c660a5bb",
+      network_id: 3
     }
+    
   }
 };
